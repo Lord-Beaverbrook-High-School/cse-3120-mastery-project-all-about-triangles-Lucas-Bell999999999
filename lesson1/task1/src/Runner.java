039d6f8cc9f347329cc0d203ca4c3175f3a5788a
38168001 <lucasb44@educbe.ca>
2024-11-14 11:39:53 -0700

    Lucas

--- lesson1/task1/src/Runner.java
+++ lesson1/task1/src/Runner.java
@@ -3,13 +3,14 @@ public class Runner {
 
         Task Triangle = new Task();
         Triangle.setSides(3.0, 4.0, 5.0);
-
+//test
         System.out.println(Triangle.perimiter());
         System.out.println(Triangle.area());
         System.out.println(Triangle.isTriangle());
         System.out.println(Triangle.isRightTriange());
 
         Triangle.desplayTriangle();
+        // prints the triangles side lenths
 
 
     }
--- lesson1/task1/src/Task.java
+++ lesson1/task1/src/Task.java
@@ -10,12 +10,14 @@ public class Task {
     double side3 = 5;
 
 public void setSides() {
+    //sets sides to a default if no lengths are provides
     side1 = 3;
     side2 = 4;
     side3 = 5;
 }
 
   public void setSides(double x, double y, double z) {
+    //uses an array to sort the sides from smallest to largest
       double[] sides = {x, y, z};
       Arrays.sort(sides);
       side1 = sides[0];
@@ -24,23 +26,28 @@ public void setSides() {
   }
 
   public boolean isTriangle() {
+    // figures out if sides can form a valid triangle by testing if the 2 smaller sides are longer when combined then the large side
       return side1 + side2 > side3;
   }
 
   public boolean isRightTriange() {
+    // uses provides formula to determine if a triangle is a right triangle
       return (round(Math.pow(side1, 2) * 100) / 100) + (round(Math.pow(side2, 2) * 100) / 100) == (round(Math.pow(side3, 2) * 100) / 100);
   }
 
  public double perimiter() {
+    // adds sides to find perimiter
     return side1 + side2 +side3;
  }
 
 public double area() {
+    //calculating ares using provided formula
     double p = perimiter() / 2;
     return Math.sqrt(p * (p - side1) * (p - side2) * (p - side3));
 }
 
 public void desplayTriangle() {
+    // printing side lengths
     System.out.println("Smallest Side " + side1);
     System.out.println("Medium Side " + side2);
     System.out.println("Largest Side " + side3);
